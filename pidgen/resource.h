7772266735d73e4324da4217e36cbc44bc6a13a3
ltsc2021 <ltsc2021@LTSC2021>
2022-09-04 15:02:58 +0800

    UPDATE

--- pidgen/resource.h
+++ pidgen/resource.h
@@ -1,12 +1,30 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by pidgen.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 pidgen.rc 使用
+//
+#define IDR_BUILD_TRIAL1                101
+#define IDR_BUILD_TRIAL2                102
+#define IDR_BUILD_TRIAL3                103
+#define IDR_BUILD_TRIAL4                104
+#define IDR_BUILD_DDK1                  105
+#define IDR_BUILD_DDK2                  106
+#define IDR_BUILD_SRV1                  107
+#define IDR_BUILD_SRV2                  108
+#define IDR_BUILD_SRV3                  109
+#define IDR_BUILD_EVAL1                 110
+#define IDR_BUILD_EVAL2                 111
+#define IDR_BUILD_PER1                  112
+#define IDR_BUILD_PER2                  113
+#define IDR_BUILD_PRO1                  114
+#define IDR_BUILD_PRO2                  115
+#define IDR_BUILD_VOL1                  116
+#define IDR_BUILD_VOL2                  117
 
-// 新对象的下一组默认值
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        118
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
