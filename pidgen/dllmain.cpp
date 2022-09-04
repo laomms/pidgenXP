b6553a3b36ce61be58808ab2a4f2fd54f6773ef5
ltsc2021 <ltsc2021@LTSC2021>
2022-09-04 14:51:40 +0800

    UPDATE

--- pidgen/dllmain.cpp
+++ pidgen/dllmain.cpp
@@ -2,20 +2,16 @@
 #include "pch.h"
 HINSTANCE g_hinst = NULL;
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(
+    HANDLE hinst,
+    ULONG ulReason,
+    LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
+    if (DLL_PROCESS_ATTACH == ulReason)
     {
-    case DLL_PROCESS_ATTACH:
-       
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+        g_hinst = (HINSTANCE)hinst;
     }
+
     return TRUE;
 }
 
